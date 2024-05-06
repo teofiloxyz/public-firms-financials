47841cae7efa9d7570c5498f7600c6d65f74724d
teofiloxyz <105755958+teofiloxyz@users.noreply.github.com>
2024-05-06 22:46:16 +0100

    add roe across companies query

---
+++ sql/queries/roe_across_companies.sql
@@ -0,0 +1,14 @@
+SELECT
+    cd.company_name,
+    cosf.net_income / bsf.total_stockholders_equity AS return_on_equity
+FROM
+    companies_dim cd
+LEFT JOIN
+    consolidated_operations_statements_fact cosf ON cd.company_id = cosf.company_id
+LEFT JOIN
+    balance_sheets_fact bsf ON cd.company_id = bsf.company_id
+WHERE
+    bsf.total_stockholders_equity > 0
+    AND cosf.net_income / bsf.total_stockholders_equity BETWEEN -2 AND 2
+ORDER BY
+    return_on_equity DESC;
